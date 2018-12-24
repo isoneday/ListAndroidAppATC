aa99f7c1aee56595e5b359fce7d51ebc9911f865
udakita <udakita11@gmail.com>
2018-12-24 14:40:35 +0700

    Initial commit

---
+++ app/src/main/java/com/training/listandroidapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.training.listandroidapp;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends ListActivity {
+
+    private TextView text;
+    private List<String> listValues;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.mainText);
+
+        listValues = new ArrayList<String>();
+        listValues.add("Android");
+        listValues.add("iOS");
+        listValues.add("KOTLIN");
+        listValues.add("REACTNATIVE");
+        listValues.add("JAVA");
+
+        // initiate the listadapter
+        ArrayAdapter<String> myAdapter = new ArrayAdapter <String>(this,
+                R.layout.row_layout, R.id.listText, listValues);
+
+        // assign the list adapter
+        setListAdapter(myAdapter);
+
+    }
+
+    // when an item of the list is clicked
+    @Override
+    protected void onListItemClick(ListView list, View view, int position, long id) {
+        super.onListItemClick(list, view, position, id);
+
+        String selectedItem = (String) getListView().getItemAtPosition(position);
+        //String selectedItem = (String) getListAdapter().getItem(position);
+
+        text.setText("You clicked " + selectedItem + " at position " + position);
+    }
+
+}
